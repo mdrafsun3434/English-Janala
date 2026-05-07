0f1842063579373d624aeccd82805318447e75d6
Rafsun <mdrafsunjani76@gmail.com>
2026-05-07 21:17:35 +0600

    done

--- script/index.js
+++ script/index.js
@@ -2,7 +2,43 @@ const loadlessons =()=>{
     fetch("https://openapi.programming-hero.com/api/levels/all") 
     .then(res=>res.json())
     .then((json)=>displayLesson(json.data));
+};
+
+const loadLevelWord =(id)=>{
+    const url =`https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+    .then(res=>res.json())
+    .then((json)=>displayLevelWord(json.data));
+};
+
+const displayLevelWord=(words)=>{
+    const wordContainer=document.getElementById("word-container");
+    wordContainer.innerHTML="";
+    for(let word of words){
+        const card=document.createElement("div");
+        card.innerHTML=`
+         <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+    <h2 class="font-bold text-xl">${word.word}</h2>
+    <p class="font-semibold">Meaning / Pronunciation</p>
+    <div class="text-2xl font-medium font-bangla">${word.meaning} / ${word.pronunciation}</div>
+    <div class="flex justify-between items-center">
+      <button class="btn bg-[#4f83b410]"><i class="fa-solid fa-circle-info"></i></button>
+      <button class="btn bg-[#4485c210]"><i class="fa-solid fa-volume-high"></i></button>
+    </div>
+  </div>
+   
+        `;
+        wordContainer.append(card);
+
+
+    }
+
+
 }
+
+
+
+
 const displayLesson=(Lessons)=>{
     console.log(Lessons);
 
@@ -12,7 +48,7 @@ const displayLesson=(Lessons)=>{
     for(let lesson of Lessons){
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML=`
-        <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book"></i> Lesson- ${lesson.level_no}</button>
+        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book"></i> Lesson- ${lesson.level_no}</button>
         `;
         levelContainer.appendChild(btnDiv);
     }
