70436bf02c3d7eb287e5c7bde658107148fa9925
Rafsun <mdrafsunjani76@gmail.com>
2026-05-07 22:40:08 +0600

    done

--- script/index.js
+++ script/index.js
@@ -14,13 +14,28 @@ const loadLevelWord =(id)=>{
 const displayLevelWord=(words)=>{
     const wordContainer=document.getElementById("word-container");
     wordContainer.innerHTML="";
+
+    if(words.length===0){
+         wordContainer.innerHTML=`
+         <div class="text-center col-span-full rounded-xl py-10 space-y-6 font-bangla ">
+         <img src="assets/alert-error.png" class="mx-auto" alt="No words available">
+        <p class="text-xl font-medium text-gray-400">এই Lesson এ এখন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="font-bold text-4xl">পরবর্তী Lesson এ যান।</h2>
+ 
+        </div>
+         
+         
+         
+         `;
+       
+    }
     for(let word of words){
         const card=document.createElement("div");
         card.innerHTML=`
          <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-    <h2 class="font-bold text-xl">${word.word}</h2>
+    <h2 class="font-bold text-xl">${word.word || 'Word not available'}</h2>
     <p class="font-semibold">Meaning / Pronunciation</p>
-    <div class="text-2xl font-medium font-bangla">${word.meaning} / ${word.pronunciation}</div>
+    <div class="text-2xl font-medium font-bangla">${word.meaning || 'Meaning not available'} / ${word.pronunciation || 'Pronunciation not available'}</div>
     <div class="flex justify-between items-center">
       <button class="btn bg-[#4f83b410]"><i class="fa-solid fa-circle-info"></i></button>
       <button class="btn bg-[#4485c210]"><i class="fa-solid fa-volume-high"></i></button>
